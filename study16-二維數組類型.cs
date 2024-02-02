104d7e5aad47ff40346750bec0afcb266a21f569
HenryChang <henry1010921@gmail.com>
2024-02-02 11:35:58 +0800

    study16

--- study16-二維數組類型.cs
+++ study16-二維數組類型.cs
@@ -7,9 +7,9 @@ namespace MyNamespace
     {
         public void statement13()
         {
-            //交錯數組  參數數組  數據類型
+            //交錯數組（不規則表格）  參數數組（params）  數據類型
 
-            //交錯數組：不規則表格
+            //交錯數組：不規則表格，每個元素都是一維數組
             int[][] array = new int[3][];//具有三個元素的交錯數組，每個元素都是一個一維數組
             //創建創建一維數組，賦值給交錯數組的每個元素
             //第一行，有....2列
@@ -18,8 +18,32 @@ namespace MyNamespace
             array[1] = new int[4];//4列
             //第三行，有....1列
             array[2] = new int[1];//1列
+            //交錯數組賦值
+            array[0][0] = 1;//第一行第一列的值為1
 
-            
+
+            //獲取交錯數組中所有元素：兩個foreach循環   
+            //是int[]，是因為array[0]是一個一維數組
+            foreach (int[] array2 in array)
+            {
+                //再取一維數組裡的值
+                foreach (int i in array2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            //array2.Length交錯數組交錯數組元素個數（可以理解為：行數）
+            for (int r = 0; r < array.Length; r++)
+            {
+                //array[r].Length交錯數組每行裡面的元素個數（可以理解為：列數）
+                for (int c = 0; c < array[r].Length; c++)
+                {
+                    Console.Write(array[r][c]+ "\t");
+                }
+                Console.WriteLine();
+            }
+
 
         }
 
